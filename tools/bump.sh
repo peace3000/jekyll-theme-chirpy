52f3f3140be7f72f45771c4800c22a2ea2dc0d7f
Peacenlove <53138211+peace3000@users.noreply.github.com>
2021-03-02 20:32:22 +0800

    Revert "Add a modification to `Gemfile.lock` in bump tool"
    This reverts commit c807282c7e4e825b5c265d7c03a6710f3db3059d.

--- tools/bump.sh
+++ tools/bump.sh
@@ -5,19 +5,15 @@
 #   - assets/js/.copyright.js
 #   - assets/js/dist/*.js (will be built by gulp later)
 #   - jekyll-theme-chirpy.gemspec
-#   - Gemfile.lock
 #   - package.json
 #
-# 2. Create a git-tag on release branch
+# 2. Create a git-tag
 #
 # 3. Build a rubygem package
-#
 #
 # Requires: gulp, rubygem
 
 set -eu
-
-auto_release=true
 
 ASSETS=(
   "_sass/jekyll-theme-chirpy.scss"
@@ -25,8 +21,6 @@ ASSETS=(
 )
 
 GEM_SPEC="jekyll-theme-chirpy.gemspec"
-
-GEM_LOCK="Gemfile.lock"
 
 NODE_META="package.json"
 
@@ -60,33 +54,26 @@ check() {
 _bump_assets() {
   _version="$1"
   for i in "${!ASSETS[@]}"; do
-    sed -i "s/v[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+/v$_version/" "${ASSETS[$i]}"
+    sed -i "s/v[[:digit:]]\.[[:digit:]]\.[[:digit:]]/v$_version/" "${ASSETS[$i]}"
   done
 
   gulp
 }
 
 _bump_gemspec() {
-  sed -i "s/[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+/$1/" "$GEM_SPEC"
+  sed -i "s/[[:digit:]]\.[[:digit:]]\.[[:digit:]]/$1/" "$GEM_SPEC"
 }
 
 _bump_node() {
   sed -i \
-    "s,[\"]version[\"]: [\"][[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+[\"],\"version\": \"$1\"," \
+    "s,[\"]version[\"]: [\"][[:digit:]]\.[[:digit:]]\.[[:digit:]][\"],\"version\": \"$1\"," \
     $NODE_META
-}
-
-_bump_gemlock() {
-  sed -i \
-    "s/jekyll-theme-chirpy ([[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+/jekyll-theme-chirpy ($1/" \
-    $GEM_LOCK
 }
 
 bump() {
   _bump_assets "$1"
   _bump_gemspec "$1"
   _bump_node "$1"
-  _bump_gemlock "$1"
 
   if [[ -n $(git status . -s) ]]; then
     git add .
@@ -156,29 +143,12 @@ main() {
     bump "$_version"
 
     echo -e "Release to v$_version\n"
-
-    if $auto_release; then
-      release "$_version"
-    fi
+    release "$_version"
 
   else
     echo "Error: Illegal version number: '$_version'"
   fi
 
 }
-
-while (($#)); do
-  opt="$1"
-  case $opt in
-    -m | --manual)
-      auto_release=false
-      shift
-      ;;
-    *)
-      echo "unknown option '$opt'!"
-      exit 1
-      ;;
-  esac
-done
 
 main
